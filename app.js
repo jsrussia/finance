075f6604a40c165bb79e396827c70f09acf3f7b1
jsrussia <dev@jsrussia.ru>
2015-07-05 22:08:56 +0300

    second commit

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ var app = express();
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(3000);
