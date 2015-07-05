73b45928bb9a8c2e7de8283f813b3075bea320d6
jsrussia <dev@jsrussia.ru>
2015-07-05 22:33:55 +0300

    [ app ] bootstrap support

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
   res.render('index');
 });
